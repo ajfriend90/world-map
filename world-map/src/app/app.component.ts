9e4de3705b54f967e524df25fc668865a49e4609
ajfriend <afri124@wgu.edu>
2025-03-24 18:33:54 -0400

    Step E, created 2 column HTML layout

--- world-map/src/app/app.component.ts
+++ world-map/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MapComponent } from './components/map/map.component';
+import { SidePanelComponent } from './components/side-panel/side-panel.component';
+import { ZoomControlsComponent } from './components/zoom-controls/zoom-controls.component';
 
 @Component({
   selector: 'app-root',
--- world-map/src/app/components/map/map.component.ts
+++ world-map/src/app/components/map/map.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { SidePanelComponent } from '../side-panel/side-panel.component';
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [],
+  imports: [SidePanelComponent],
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
